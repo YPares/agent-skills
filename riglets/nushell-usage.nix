55a218b2759c94fa0ea573d3a603f0c45ccf7685
Yves Pares <yves.pares@aletiq.com>
2026-01-09 01:55:12 +0100

    nushell-usage: added option to add nu MCP server

--- riglets/nushell-usage.nix
+++ riglets/nushell-usage.nix
@@ -1,9 +1,20 @@
 self:
-{ system, riglib, ... }:
+{
+  system,
+  riglib,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
   inherit (self.inputs.nushellWith.packages.${system}) nushellMCP;
 in
 {
+  options.nushell-usage = {
+    withMcp = riglib.options.flag "Add the nushell MCP server to the rig";
+  };
+
   config.riglets.nushell-usage = {
     tools = [ nushellMCP ];
 
@@ -34,5 +45,10 @@ in
       # If does not exist, nu will try to create it and it will fail because in the rig, XDG_CONFIG_HOME is readonly
       nushell."config.nu" = "";
     };
+  }
+  // lib.optionalAttrs config.nushell-usage.withMcp {
+    mcpServers.nushell.command = pkgs.writeShellScriptBin "nu-mcp" ''
+      ${lib.getExe nushellMCP} --mcp "$@"
+    '';
   };
 }
